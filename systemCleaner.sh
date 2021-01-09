1d42338f6c59b78b4e991abe6d0cb1b4c3984310
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-01-09 12:48:26 +0100

    initial commit

---
+++ systemCleaner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+echo "Cache apt..."
+sudo du -sh /var/cache/apt 
+
+echo "Cleaning with autoremove, autoclean and clean"
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y clean
+
+echo "Before cleaning"
+du -h /var/lib/snapd/snaps
+
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+echo "After cleaning..."
+du -h /var/lib/snapd/snaps
+
+
+echo "thumbnails"
+du -sh ~/.cache/thumbnails
+rm -rf ~/.cache/thumbnails/*
+
+
