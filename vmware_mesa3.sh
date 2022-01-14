31d5c1580f91b5345de60ffc63087b69aa04b097
Jose Ramon Mañes <32740567+jrmanes@users.noreply.github.com>
2022-01-14 13:35:07 +0100

    keep only one apt

--- vmware_mesa3.sh
+++ vmware_mesa3.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-
-sudo apt-get install git-core
-sudo apt-get install ninja-build meson libpthread-stubs0-dev
-sudo apt-get install xserver-xorg-dev x11proto-xinerama-dev libx11-xcb-dev
-sudo apt-get install libxcb-glx0-dev libxrender-dev
-sudo apt-get build-dep libgl1-mesa-dri libxcb-glx0-dev
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git-core \
+    ninja-build \
+    meson \
+    libpthread-stubs0-dev \
+    xserver-xorg-dev \
+    x11proto-xinerama-dev \
+    libx11-xcb-dev \  
+    libxcb-glx0-dev \
+    libxrender-dev \
+    build-dep \
+    libgl1-mesa-dri \
+    libxcb-glx0-dev
